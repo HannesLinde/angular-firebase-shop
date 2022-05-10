f5b8813bb05399825e46bbc51076001089bfe06f
user <ilyes.mansour.3@gmail.com>
2022-05-10 20:36:23 +0200

    add core and share modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
+//Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { environment } from 'src/environments/environment';
 
+//Modules
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
-import {MaterialModule} from './material.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Components
+import { AppComponent } from './app.component';
+
+//Directives
+
+//Pipes
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     BrowserAnimationsModule,
-    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
