370a7db94a5473b58ed652c501ae1352ac5cb5e7
HannesLinde <hanneslindenberg@posteo.de>
2022-05-10 12:02:42 +0200

    Basic config testing and ng material
    - Unit testing: add jest, remove Karma
    - e2e testing: add cypress
    - material ui: create src/app/material.module and import it on root level

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {MaterialModule} from './material.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
