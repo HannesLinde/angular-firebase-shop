6d820390901c569adf23155d00002f5f132e6d03
user <ilyes.mansour.3@gmail.com>
2022-05-10 21:12:31 +0200

    update nav style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Firestore,collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
