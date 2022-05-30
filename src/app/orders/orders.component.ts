7fcb50483db4aea0e91e3200130003f70527ddfa
user <ilyes.mansour.3@gmail.com>
2022-05-30 15:43:32 +0200

    display detail on dialog

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { Observable, of } from 'rxjs';
+import { AppState } from '@app/store/reducers/app.reducer';
+import { getHandSet } from '@app/store/selectors/app.selector';
+import { Store } from '@ngrx/store';
+import { Observable, of, Subscription, tap } from 'rxjs';
 import { Order } from './models/order.model';
 import { OrderDetailDialogComponent } from './order-detail-dialog/order-detail-dialog.component';
 import { OrdersService } from './orders.service';
@@ -11,20 +14,29 @@ import { OrdersService } from './orders.service';
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css'],
 })
-export class OrdersComponent implements OnInit {
+export class OrdersComponent implements OnInit, OnDestroy {
   orders$!: Observable<Order[]>;
+  isHandSet = false;
   selectedOrder!: Order;
-  constructor(private orderService: OrdersService, private dialog: MatDialog) {}
+  subscription = new Subscription();
+
+  constructor(private orderService: OrdersService, private dialog: MatDialog, private appStore: Store<AppState>) {}
 
   ngOnInit(): void {
     this.orders$ = this.orderService.getAll();
+    this.subscription.add(this.appStore.select(getHandSet).subscribe((isHandSet) => (this.isHandSet = isHandSet)));
   }
 
   onOrderSelected(order: Order) {
     this.selectedOrder = order;
-    /*let dialogRef = this.dialog.open(OrderDetailDialogComponent, {
-      width: '450px',
-      data: order,
-    });*/
+    if (this.isHandSet)
+      this.dialog.open(OrderDetailDialogComponent, {
+        width: '450px',
+        data: order,
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
